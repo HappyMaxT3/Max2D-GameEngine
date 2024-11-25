082e96432bf3f81df30da1ce2ed702e856f36ca4
HappyMaxT3 <m9775531181@gmail.com>
2024-11-25 14:58:54 +0300

    fixed errors (working start demo window)

--- Max2D-GameEngine/DemoGame.cs
+++ Max2D-GameEngine/DemoGame.cs
@@ -3,7 +3,7 @@ using Max2D_GameEngine.GameEngine;
 
 namespace Max2D_GameEngine
 {
-    class DemoGame : Max2D_GameEngine.GameEngine
+    class DemoGame : GameEngine.GameEngine
     {
         public DemoGame() : base(new Vector2(615, 515), "2d Game DEMO") { }
 
--- Max2D-GameEngine/GameEngine/GameEngine.cs
+++ Max2D-GameEngine/GameEngine/GameEngine.cs
@@ -3,15 +3,19 @@ using System.Windows.Forms;
 
 namespace Max2D_GameEngine.GameEngine
 {
-	class Canvas : Form
-	{
-		public Canvas()
-		{
-			this.DoubleBuffered = true;
-		}
-	}
+    class Canvas : Form
+    {
+        public Canvas()
+        {
+            this.DoubleBuffered = true;
+        }
 
-	public abstract class GameEngineClass
+        //private void InitializeComponent()
+        //{
+        //}
+    }
+
+    public abstract class GameEngine
 	{
 		public Vector2 ScreenSize = new Vector2(512, 512);
 		private string Title = "GameTitle";
