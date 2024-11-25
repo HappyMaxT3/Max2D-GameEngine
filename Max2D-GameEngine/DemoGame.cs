aa939a286f5832ac69cefeaff4fc5dec96072e81
HappyMaxT3 <m9775531181@gmail.com>
2024-11-25 15:34:06 +0300

    added base game loops

--- Max2D-GameEngine/DemoGame.cs
+++ Max2D-GameEngine/DemoGame.cs
@@ -7,5 +7,22 @@ namespace Max2D_GameEngine
     {
         public DemoGame() : base(new Vector2(615, 515), "2d Game DEMO") { }
 
+        public override void OnDraw()
+        {
+            Console.WriteLine("OnDraw working");
+        }
+
+        public override void OnLoad()
+        {
+            Console.WriteLine("OnLoad working");
+        }
+
+        int frame = 0;
+        public override void OnUpdate()
+        {
+            Console.WriteLine($"frame count: {frame}");
+            frame++;
+        }
+
     }
 }
--- Max2D-GameEngine/GameEngine/GameEngine.cs
+++ Max2D-GameEngine/GameEngine/GameEngine.cs
@@ -12,6 +12,7 @@ namespace Max2D_GameEngine.GameEngine
 
         //private void InitializeComponent()
         //{
+
         //}
     }
 
@@ -20,6 +21,7 @@ namespace Max2D_GameEngine.GameEngine
 		public Vector2 ScreenSize = new Vector2(512, 512);
 		private string Title = "GameTitle";
 		private Canvas Window = null;
+        private Thread GameLoopThread = null;
 
 		public GameEngine(Vector2 ScreenSize, string Title)
 		{
@@ -29,9 +31,46 @@ namespace Max2D_GameEngine.GameEngine
 			Window = new Canvas();
 			Window.Size = new Size((int)this.ScreenSize.X, (int)this.ScreenSize.Y);
 			Window.Text = this.Title;
+            Window.Paint += Renderer;
+
+			
+
+            GameLoopThread = new Thread(GameLoop);
+            GameLoopThread.Start();
 
 			Application.Run(Window);
 		}
+
+        private void GameLoop()
+        {
+            OnLoad();
+
+            while (GameLoopThread.IsAlive) 
+            {
+                try
+                {
+                    OnDraw();
+                    Window.BeginInvoke((MethodInvoker)delegate { Window.Refresh(); });
+                    OnUpdate();
+                    Thread.Sleep(5);
+                }
+                catch
+                {
+                    Console.WriteLine("game is loading...");
+                }
+
+            }
+
+        }
+
+        private void Renderer(object? sender, PaintEventArgs e)
+        {
+            Graphics g =e.Graphics;
+        }
+
+        public abstract void OnLoad();
+        public abstract void OnUpdate();
+        public abstract void OnDraw();
 
 	}
 
