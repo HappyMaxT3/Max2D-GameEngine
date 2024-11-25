fe20f4df7bc1a8f52d5b71c4ce95d247d4febd10
Max <m9775531181@gmail.com>
2024-11-25 13:42:00 +0300

    formed window

--- Max2D-GameEngine/GameEngine/GameEngineClass.cs
+++ Max2D-GameEngine/GameEngine/GameEngineClass.cs
@@ -14,12 +14,19 @@ namespace Max2D_GameEngine.GameEngine
 	public abstract class GameEngineClass
 	{
 		public Vector2 ScreenSize = new Vector2(512, 512);
-		private string Title = "jd";
+		private string Title = "Game Title";
+		private Canvas Window = null;
 
 		public GameEngine(Vector2 ScreenSize, string Title)
 		{
 			this.ScreenSize = ScreenSize;
 			this.Title = Title;
+
+			Window = new Canvas();
+			Window.Size = new Size((int)this.ScreenSize.X, (int)this.ScreenSize.Y);
+			Window.Text = this.Title;
+
+			Application.Run(Window);
 		}
 
 	}
